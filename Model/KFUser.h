8509978f7e56c78e13ab61c5629f1b766359c50b
yanchaoGan <2429768907@qq.com>
2014-11-21 14:26:10 +0800

    aa

---
+++ Model/KFUser.h
@@ -0,0 +1,13 @@
+//
+//  KFUser.h
+//  KFManagerSystem
+//
+//  Created by admin on 14-11-21.
+//  Copyright (c) 2014年 gychao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KFUser : NSObject
+
+@end
---
+++ Model/KFUser.m
@@ -0,0 +1,13 @@
+//
+//  KFUser.m
+//  KFManagerSystem
+//
+//  Created by admin on 14-11-21.
+//  Copyright (c) 2014年 gychao. All rights reserved.
+//
+
+#import "KFUser.h"
+
+@implementation KFUser
+
+@end
