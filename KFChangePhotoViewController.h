4c59a4bbc1a016a12086352430af1f2229598de3
yanchaoGan <2429768907@qq.com>
2014-11-26 18:31:44 +0800

    aa

---
+++ KFChangePhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  KFChangePhotoViewController.h
+//  KFManagerSystem
+//
+//  Created by admin on 14-11-26.
+//  Copyright (c) 2014年 gychao. All rights reserved.
+//
+
+#import "KFBaseViewController.h"
+
+@interface KFChangePhotoViewController : KFBaseViewController
+
+@end
---
+++ KFChangePhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  KFChangePhotoViewController.m
+//  KFManagerSystem
+//
+//  Created by admin on 14-11-26.
+//  Copyright (c) 2014年 gychao. All rights reserved.
+//
+
+#import "KFChangePhotoViewController.h"
+
+@interface KFChangePhotoViewController ()
+
+@end
+
+@implementation KFChangePhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
